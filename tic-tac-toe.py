c30666fe164eb8e6332188ab22e725ce281c25a9
Mary Goff <maryrgoff@gmail.com>
2022-01-11 20:52:03 -0700

    added instruction comments to file

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,1 +1,7 @@
+#Tic-Tac-Toe rules.
 
+#1. The game is played on a 3x3 grid.
+#2. Player one uses x's. Player two uses o's.
+#3. Players take turns putting their marks in empty squares.
+#4. First to 3 marks in a row (vertical, horizontal, or diagonal) wins.
+#5. If the game board is full, and no one won, it't a tie.
