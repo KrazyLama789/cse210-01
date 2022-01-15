fc4e6c037f76d7ec0e1bc8685b60202619a3badb
Mary Goff <maryrgoff@gmail.com>
2022-01-14 20:00:19 -0700

    fully funtioning tic-tak-toe itteration 1

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,26 +1,67 @@
-#Tic-Tac-Toe rules.
+# The program must have a comment with assignment (make a funcitoning Tic-Tac-Toe game) and author names (Mary Goff).
+# The program must have at least one if/then block.
+# The program must have at least one while loop.
+# The program must have more than one function.
+# The program must have a function called main.
 
-#1. The game is played on a 3x3 grid.
-#2. Player one uses x's. Player two uses o's.
-#3. Players take turns putting their marks in empty squares.
-#4. First to 3 marks in a row (vertical, horizontal, or diagonal) wins.
-#5. If the game board is full, and no one won, it't a tie.
+# A function with a refreshable game board.
+def gameboard(numberlist):
+    one, two, three, four, five, six, seven, eight, nine = numberlist
+    gameboard = (
+        f"\n{one}|{two}|{three} \n-+-+- \n{four}|{five}|{six} \n-+-+- \n{seven}|{eight}|{nine} \n")
+    return gameboard
+# A function to evaluate win conditions, and return the result.
+
+def winconditions(numberlist, turn):
+    win = False
+    if numberlist[0] == turn and numberlist[1] == turn and numberlist[2] == turn:
+        win = True
+    elif numberlist[3] == turn and numberlist[4] == turn and numberlist[5] == turn:
+        win = True
+    elif numberlist[6] == turn and numberlist[7] == turn and numberlist[8] == turn:
+        win = True
+    elif numberlist[0] == turn and numberlist[3] == turn and numberlist[6] == turn:
+        win = True
+    elif numberlist[1] == turn and numberlist[4] == turn and numberlist[7] == turn:
+        win = True
+    elif numberlist[2] == turn and numberlist[5] == turn and numberlist[8] == turn:
+        win = True
+    elif numberlist[0] == turn and numberlist[4] == turn and numberlist[8] == turn:
+        win = True
+    elif numberlist[6] == turn and numberlist[4] == turn and numberlist[2] == turn:
+        win = True
+    else:
+        return
+    return win
+# A funciton to play the game of Tic-Tak-Toe
 
 def main():
     turncount = 0
-    one = 1
-    two = 2
-    three = 3
-    four = 4
-    five = 5
-    six = 6
-    seven = 7
-    eight = 8
-    nine = 9
-    gameboard = (f"{one}|{two}|{three} \n-+-+- \n{four}|{five}|{six} \n-+-+- \n{seven}|{eight}|{nine} \n")
-    print(gameboard)
-    while turncount <= 9:
+    numberlist = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    print("\nWelcome to Tic-Tac-Toe")
+    while turncount < 9:
+        print(gameboard(numberlist))
+        turn = ""
+        if turncount % 2 == 0:
+            turn = "X"
+            turninput = int(input("It's X's turn. Choose a number(1-9):\n"))
+            if type(numberlist[turninput-1]) == int:
+                numberlist[turninput-1] = "X"
+                if winconditions(numberlist, turn) == True:
+                    print(gameboard(numberlist))
+                    return print(f"{turn} Wins! Thanks for playing!")
+            else:
+                print("\nThat space is already taken. Try again.")
+                turncount -= 1
+        else:
+            turn = "O"
+            turninput = int(input("It's O's turn. Choose a number(1-9):\n"))
+            if type(numberlist[turninput-1]) == int:
+                numberlist[turninput-1] = "O"
+                if winconditions(numberlist, turn) == True:
+                    return print(f"{turn} Wins! Thanks for playing!")
+            else:
+                print("\nThat space is already taken. Try again.")
+                turncount -= 1
         turncount += 1
-    print("turn 9")
-    
 main()
